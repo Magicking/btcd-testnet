39c4628158691352df853c187f9b4f54efa35148
Magicking <s@6120.eu>
2018-02-13 17:16:12 +0100

    Add addrindex

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cmd="btcd -C /btcd/btcd.conf"
+cmd="btcd -C /btcd/btcd.conf --addrindex"
 
 [ -n "$BTCD_LIMITUSER" ] && cmd="$cmd --rpclimituser=${BTCD_LIMITUSER}"
 [ -n "$BTCD_LIMITPASS" ] && cmd="$cmd --rpclimitpass=${BTCD_LIMITPASS}"
